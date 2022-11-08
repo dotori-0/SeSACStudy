60957335a35a3e12bad40db112d49f7ebab36d2d
s <sesan131@gmail.com>
2022-11-09 02:30:21 +0900

    [Add] #5 Custom Font Files

---
+++ SeSACStudy/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  SeSACStudy
+//
+//  Created by SC on 2022/11/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        for family in UIFont.familyNames {
+            print("====\(family)====")
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
