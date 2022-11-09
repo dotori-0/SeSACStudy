48966cce70097d338bf919050bbac14f882d970e
dotori-0 <sesan131@gmail.com>
2022-11-10 00:56:06 +0900

    [Add] GlobalButton

---
+++ SeSACStudy/Utilities/Custom/GlobalButton.swift
@@ -0,0 +1,43 @@
+//
+//  GlobalButton.swift
+//  SeSACStudy
+//
+//  Created by SC on 2022/11/10.
+//
+
+import UIKit
+
+class GlobalButton: UIButton {
+    // MARK: - Properties
+    var title: String?
+
+    // MARK: - Initializers
+    init(title: String) {
+        self.title = title
+        super.init(frame: .zero)
+        
+        setUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Design Methods
+    private func setUI() {
+        guard let title = title else {
+            print("No title for GlobalButton")
+            return
+        }
+
+        var container = AttributeContainer()
+        container.font = .Body3_R14
+//        container.foregroundColor = Asset.Colors.BlackWhite.white
+        
+        var buttonConfiguration = UIButton.Configuration.filled()
+        buttonConfiguration.baseBackgroundColor = Asset.Colors.BrandColor.green
+        buttonConfiguration.attributedTitle = AttributedString(title, attributes: container)
+        
+        configuration = buttonConfiguration
+    }
+}
