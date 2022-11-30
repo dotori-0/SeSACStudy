3532d90a4902b01775c270fd0c9dad28a04d4e41
dotori-0 <sesan131@gmail.com>
2022-12-01 01:53:04 +0900

    [Add] BaseTableViewCell

---
+++ SeSACStudy/Utilities/Base/BaseTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  BaseTableViewCell.swift
+//  SeSACStudy
+//
+//  Created by SC on 2022/12/01.
+//
+
+import UIKit
+
+class BaseTableViewCell: UITableViewCell {
+    // MARK: - Initializers
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setUI()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Design Methods
+    func setUI() { }
+    
+    func setConstraints() { }
+}
